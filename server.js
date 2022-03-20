33a5331705cb40ed1c8d7204534e1828ee368446
rharris529 <jordandine@gmail.com>
2022-03-20 15:18:02 -0500

    create connection to db and express

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // mySQL username
+        user: 'root',
+        // mySQL pass
+        password: 'falconpunch@++@kk90',
+        database: 'election',
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (NOT FOUND)
 app.use((req,res) => {
