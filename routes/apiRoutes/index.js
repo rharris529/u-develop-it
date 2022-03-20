de760ea28c90d776f89cf4234bb9a9601a9c0687
rharris529 <jordandine@gmail.com>
2022-03-20 18:16:55 -0500

    added vote routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use(require('./candidateRoute'));
 router.use(require('./partyRoute'));
 router.use(require('./voterRoute'));
+router.use(require('./voteRoutes'));
 
 module.exports = router;
